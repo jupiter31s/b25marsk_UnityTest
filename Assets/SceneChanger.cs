52427cf8cbbc1a6095a22f7dbc913ab1bcd679b4
SidneyEerselsPXL <95049072+SidneyEerselsPXL@users.noreply.github.com>
2026-01-08 12:04:09 +0100

    for youu <3

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("level1");
+    }
+}
